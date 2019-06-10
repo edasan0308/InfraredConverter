1f7a821d3e80cc709b8320d1740a4ea57ed9ab8d
edasan0308 <edasan0568@gmail.com>
2019-06-10 21:54:31 +0900

    INSERT PROJECT

---
+++ Project.py
@@ -0,0 +1,64 @@
+import difflib
+# ファイルをオープンする
+test_data = open("/Users/k17017kk/python/project/inputcodes.txt", "r")
+in_data = open("/Users/k17017kk/python/project/input.txt", "r")
+inline = in_data.readline()
+sin_Data = inline.split(":")
+sin_Data[1] = sin_Data[1].lstrip(" [")
+sin_Data[1] = sin_Data[1].rstrip("\n")
+sin_Data[1] = sin_Data[1].rstrip("}")
+sin_Data[1] = sin_Data[1].rstrip(",")
+sin_Data[1] = sin_Data[1].rstrip("]")
+input = sin_Data[1]
+SplitData2 = []
+for b in range(112):
+    SplitData2.append(input.split(",")[b])
+
+print("input")
+print(input)
+
+# 行ごとにすべて読み込んでリストデータにする
+lines = test_data.readlines()
+
+Datalist = [[]]
+s = []
+SplitData = []
+# 一行ずつ表示する
+for i,line in enumerate(lines):
+    s_Data = line.split(":")
+    #Datalist = [x for x in Datalist if x]
+    Datalist.append([s_Data[0],s_Data[1]]);
+    Datalist = [x for x in Datalist if x]
+
+    Datalist[i][0] = Datalist[i][0].strip("{")
+    Datalist[i][0] = Datalist[i][0].strip(" \"")
+    Datalist[i][1] = Datalist[i][1].lstrip(" [")
+    Datalist[i][1] = Datalist[i][1].rstrip("\n")
+    Datalist[i][1] = Datalist[i][1].rstrip("}")
+    Datalist[i][1] = Datalist[i][1].rstrip(",")
+    Datalist[i][1] = Datalist[i][1].rstrip("]")
+
+    SplitData.append(Datalist[i][1].split(",",112))
+
+    print(Datalist[i][0])
+    print(Datalist[i][1])
+
+print()
+
+Dis = []
+for i,data in enumerate(Datalist):
+    Dis.append(0)
+    #0クリア
+
+    for b in range(111):
+        Dis[i] += abs(int(SplitData[i][b]) - int(SplitData2[b]) )
+    #s.append(difflib.SequenceMatcher(None, input, Datalist[i][1]).ratio())
+    #それぞれの信号のシンクロ率の算出
+
+    print(Datalist[i][0]+"    "+str(Dis[i]))
+
+print()
+print(Datalist[Dis.index(min(Dis))][0]+"    "+str(min(Dis)))
+# ファイルをクローズする
+test_data.close()
+in_data.close()
